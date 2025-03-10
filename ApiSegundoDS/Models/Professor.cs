17da6d26eac2f5f3cab8db40d5e010aa74962b7d
Akirazzzzz <anikoramok@gmail.com>
2025-03-10 09:07:08 -0300

    feat: Classes escolas, professor e aluno

---
+++ ApiSegundoDS/Models/Professor.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string? Bagi { get; set; }
+        public string? RNA { get; set; }
+        public string? Pai { get; set; }
+        public string? Mae { get; set; }
+        public Escola Escola { get; set; }
+        public Aluno Aluno{ get; set; }
+    }
+}
