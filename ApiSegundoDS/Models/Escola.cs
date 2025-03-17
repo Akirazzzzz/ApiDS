3a473eb45a0fd334de060b17b8465008072c726f
Akirazzzzz <anikoramok@gmail.com>
2025-03-17 08:18:17 -0300

    feat:atualização

--- ApiSegundoDS/Models/Escola.cs
+++ ApiSegundoDS/Models/Escola.cs
@@ -3,7 +3,9 @@
     public class Escola
     {
         public int Id { get; set; }
-        public string? Bagi { get; set; }
-        public Professor Professor { get; set; }
+        public string? Nome { get; set; }
+        public string? CNPJ { get; set; }
+        public Aluno? Aluno { get; set; }
+        public Professor? Professor { get; set; }
     }
 }
--- ApiSegundoDS/Models/Professor.cs
+++ ApiSegundoDS/Models/Professor.cs
@@ -7,6 +7,7 @@
         public string? RNA { get; set; }
         public string? Pai { get; set; }
         public string? Mae { get; set; }
+        public string? ComponenteCurricular { get; set; }
         public Escola Escola { get; set; }
         public Aluno Aluno{ get; set; }
     }
