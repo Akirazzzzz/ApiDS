bc1f438b758f14f2958782a81169ff7157cd1dcc
Akirazzzzz <anikoramok@gmail.com>
2025-03-24 09:04:52 -0300

    feat: funcionando

--- ApiSegundoDS/Models/Professor.cs
+++ ApiSegundoDS/Models/Professor.cs
@@ -3,12 +3,12 @@
     public class Professor
     {
         public int Id { get; set; }
-        public string? Bagi { get; set; }
+        public string? Nome { get; set; }
         public string? RNA { get; set; }
         public string? Pai { get; set; }
         public string? Mae { get; set; }
         public string? ComponenteCurricular { get; set; }
         public int EscolaID { get; set; }
-        public int AlunoID{ get; set; }
+        public int AlunoID { get; set; }
     }
 }
